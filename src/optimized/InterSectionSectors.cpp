e48ebf5b52fd5409c1cad7fa099d1f64a3c68c7d
SavaLione <monologuesplus@gmail.com>
2019-03-06 20:18:56 +0500

    cxxopts

--- src/optimized/InterSectionSectors.cpp
+++ src/optimized/InterSectionSectors.cpp
@@ -12,13 +12,131 @@
 
 #include <stdio.h>
 #include <time.h>
+#include <iostream>
 
+#include "cxxopts.hpp"
 #include "ReadFile.h"
 #include "Sector.h"
 #include "rand_sse.h"
 
 using namespace std;
 
+void InterSectionSectors();
+
+int main(int argc, char *argv[])
+{
+	bool hide_log = false, time_test = false;
+	int i_accuracy = 1000;
+	
+	cxxopts::Options options("InterSectionSectors", " - Inter Section Sectors");
+	options.add_options()
+		("h,help", 		"Help"											)
+		("l,log", 		"Log", 			cxxopts::value<bool>(hide_log)	)
+		("a,accuracy", 	"Accuracy", 	cxxopts::value<int>(), "N"		)
+		("t,time", 		"Time test", 	cxxopts::value<bool>(time_test)	)
+		("p,parallel", 	"Parallel info"									);
+	
+	auto result = options.parse(argc, argv);
+	
+	if(result.count("help"))
+    {
+      std::cout << options.help({"", "Group"}) << std::endl;
+      exit(0);
+    }
+	
+	if(hide_log)
+	{
+		std::cout << "HIDE LOG" << std::endl;
+	}
+	
+	if(time_test)
+	{
+		std::cout << "TIME TEST" << std::endl;
+	}
+	
+	if(result.count("parallel"))
+	{
+	#ifdef _OPENMP
+		#pragma omp parallel
+		{
+			printf("PARALLEL\n");
+		}
+		printf("Compiled by an OpenMP-compliant implementation.\n");
+	#else
+		printf("Compiled without OpenMP\n");
+	#endif
+	exit(0);
+	}
+	
+	if(result.count("accuracy"))
+	{
+		i_accuracy = result["accuracy"].as<int>();
+		std::cout << "Accuracy = " << i_accuracy << std::endl;
+	}
+	
+	InterSectionSectors();
+	
+	return 0;
+}
+
+void InterSectionSectors()
+{
+#if TIME_TEST
+	std::chrono::time_point<std::chrono::system_clock> start, end;
+    start = std::chrono::system_clock::now();
+#endif
+	srand_sse(time(NULL)); // seed
+
+    int count_set = 0;
+    int count_intersection = 0;
+	Point temp_point;
+#if DATA_OUTPUT
+	double radius = 0;
+#endif
+
+	vector<vector<Sector>> data;
+	data = ReadDataFromFile((char*)"data.test");
+
+	for(vector<vector<Sector> >::iterator it = data.begin(); it != data.end(); ++it)
+    {
+#if DATA_OUTPUT
+		printf("Number set == %i\n", count_set);
+        PrintSetSectors(*it);
+#endif
+        temp_point = CheckIntersectionSetOfSectors(*it);
+
+        if (temp_point.x == 0 && temp_point.y == 0)
+        {
+#if DATA_OUTPUT
+			printf("Intesection of Sectors Not Found!\n");
+#endif
+        }
+        else
+        {
+#if DATA_OUTPUT
+			radius = 0;
+            Point center_circle = CreateCircleFromArea(temp_point, *it, &radius);
+			printf("Center Circle -- (%lf ; %lf ),    Radius = %lf\n", center_circle.x, center_circle.y, radius);
+#endif
+            count_intersection++;
+        }
+#if DATA_OUTPUT
+        printf("\n\n");
+#endif
+
+		count_set++;
+    }
+    printf("\n\nCount of Intersection === %i", count_intersection);
+
+#if TIME_TEST
+	end = std::chrono::system_clock::now();
+    int elapsed_seconds = std::chrono::duration_cast<std::chrono::seconds> (end-start).count();
+	printf("\n\n\n\nTIME\t%i\n\n", elapsed_seconds);
+#endif
+}
+
+/* DO NOT DELETE */
+/*
 int main()
 {
 #if PARALLEL
@@ -85,3 +203,4 @@ int main()
 #endif
 	return 0;
 }
+*/
